12973e01d64bb9086e280941a925ad6c3e24e7ec
jtam97 <justintam97@gmail.com>
2024-09-25 11:36:02 -0400

    updated experiences, skills, about and added new CV file

--- index.js
+++ index.js
@@ -3,20 +3,20 @@
  ---------------------------------------- */
 
 const handleFirstTab = (e) => {
-  if(e.key === 'Tab') {
-    document.body.classList.add('user-is-tabbing')
+	if (e.key === 'Tab') {
+		document.body.classList.add('user-is-tabbing')
 
-    window.removeEventListener('keydown', handleFirstTab)
-    window.addEventListener('mousedown', handleMouseDownOnce)
-  }
+		window.removeEventListener('keydown', handleFirstTab)
+		window.addEventListener('mousedown', handleMouseDownOnce)
+	}
 
 }
 
 const handleMouseDownOnce = () => {
-  document.body.classList.remove('user-is-tabbing')
+	document.body.classList.remove('user-is-tabbing')
 
-  window.removeEventListener('mousedown', handleMouseDownOnce)
-  window.addEventListener('keydown', handleFirstTab)
+	window.removeEventListener('mousedown', handleMouseDownOnce)
+	window.addEventListener('keydown', handleFirstTab)
 }
 
 window.addEventListener('keydown', handleFirstTab)
@@ -25,19 +25,19 @@ const backToTopButton = document.querySelector(".back-to-top");
 let isBackToTopRendered = false;
 
 let alterStyles = (isBackToTopRendered) => {
-  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
-  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
-  backToTopButton.style.transform = isBackToTopRendered
-    ? "scale(1)"
-    : "scale(0)";
+	backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+	backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+	backToTopButton.style.transform = isBackToTopRendered
+		? "scale(1)"
+		: "scale(0)";
 };
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 700) {
-    isBackToTopRendered = true;
-    alterStyles(isBackToTopRendered);
-  } else {
-    isBackToTopRendered = false;
-    alterStyles(isBackToTopRendered);
-  }
+	if (window.scrollY > 700) {
+		isBackToTopRendered = true;
+		alterStyles(isBackToTopRendered);
+	} else {
+		isBackToTopRendered = false;
+		alterStyles(isBackToTopRendered);
+	}
 });
